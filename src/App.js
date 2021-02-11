3e2fdf572abcb9350543ca830994db7a1058e392
iamsaif <saifulhasan580@gmail.com>
2021-02-11 21:13:10 +0530

    About functional based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+//Import Person from Person folder
+import Person from './Person/Person'
 
+//Class based components and functional components
 function App() {
   return (
+    //Class should be written as className i.e camel case 
+    //There should be only one root element
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello world!! My name is Saif</h1>
+      <p>This is  really working!</p>
+      <Person />
     </div>
   );
+  // return React.createElement('div',{className:'App'},React.createElement('h1',null,'Hello my anme is saiful hasan'))
 }
 
 export default App;
---
+++ src/Person/Person.js
@@ -0,0 +1,9 @@
+//Functional based Component 
+//function can be created using es5 or es6
+const Person=()=>{
+    return (
+        <p>I'm a person!</p>
+    )
+}
+
+export default Person;
