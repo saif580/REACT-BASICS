b08d4d515a47783a9a99497811550ec29eb8ed17
iamsaif <saifulhasan580@gmail.com>
2021-02-12 15:09:22 +0530

    basic styling

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,12 +1,13 @@
+import './Person.css'
 //Functional based Component 
 //function can be created using es5 or es6
 const Person=(props)=>{
     //props.children it is a reserve word to access children of a particular component
     return (
-        <div>
+        <div className="Person">
             <p onClick={props.click}>I'm a person! and my name is {props.name} and I have {props.experience} yrs of expirence</p>
             <p>{props.children}</p>
-            <input type="text" onChange={props.changed} value={this.name}/>
+            <input type="text" onChange={props.changed} value={props.name}/>
         </div>
     )
 }
